8a327367ab456890c80227b0eeacee6084bdfb21
Ayushee Shrivastava <aayushee.shrivastava@gmail.com>
2020-12-14 22:31:59 +0530

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+function validateEmail() {
+   
+    const image = document.createElement("img");
+    image.src="images/icon-error.svg";
+    document.querySelector("button").append(image).style.marginLeft = "40px";
+    
+ }
